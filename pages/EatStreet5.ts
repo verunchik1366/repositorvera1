6d3c619f1b93f8f1aaef8081fe89feff04742aa0
verunchik1366 <verunchik1366@gmail.com>
2025-08-06 09:37:33 +0300

    тест 5  іт стріт

---
+++ pages/EatStreet5.ts
@@ -0,0 +1,37 @@
+import { Page, Locator, expect } from '@playwright/test'; // Додано expect в імпорти
+
+export class EatStreet5 {
+  readonly page: Page;
+  readonly saveBigContainer: Locator;
+  readonly emailInput: Locator;
+  readonly nameInput: Locator;
+  readonly checkbox: Locator;
+  readonly submitButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    
+    // Ініціалізація локаторів
+    this.saveBigContainer = page.locator('div.container--fluid:has-text("Save Big, Eat Happy!")');
+    this.emailInput = page.locator('input#new-email');
+    this.nameInput = page.locator('input#new-name');
+    this.checkbox = page.locator('input[type="checkbox"]');
+    this.submitButton = page.locator('button[type="submit"]');
+  }
+
+  async verifyAllElements() {
+    // Очікування появи елементів
+    await this.saveBigContainer.waitFor();
+    await this.emailInput.waitFor();
+    await this.nameInput.waitFor();
+    await this.checkbox.waitFor();
+    await this.submitButton.waitFor();
+
+    // Перевірка видимості
+    await expect(this.saveBigContainer).toBeVisible();
+    await expect(this.emailInput).toBeVisible();
+    await expect(this.nameInput).toBeVisible();
+    await expect(this.checkbox).toBeVisible();
+    await expect(this.submitButton).toBeVisible();
+  }
+}
